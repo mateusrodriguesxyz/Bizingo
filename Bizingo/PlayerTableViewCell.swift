2e6533e9d30251e2b2cad1342d070dbf96943080
Mateus Rodrigues <mateus.fr26@gmail.com>
2020-02-10 17:59:14 -0300

    Mon Feb 10 17:59:14 -03 2020

---
+++ Bizingo/PlayerTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PlayerTableViewCell.swift
+//  Bizingo
+//
+//  Created by Mateus Rodrigues on 10/02/20.
+//  Copyright © 2020 Mateus Rodrigues. All rights reserved.
+//
+
+import UIKit
+
+class PlayerTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ Bizingo/SCKManager.swift
@@ -0,0 +1,13 @@
+//
+//  SCKManager.swift
+//  Bizingo
+//
+//  Created by Mateus Rodrigues on 10/02/20.
+//  Copyright © 2020 Mateus Rodrigues. All rights reserved.
+//
+
+import UIKit
+
+class SCKManager: NSObject {
+
+}
